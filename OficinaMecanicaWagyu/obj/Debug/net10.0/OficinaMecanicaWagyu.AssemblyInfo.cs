65a46d9bc566c551dbe36219adf5b5732599451e
Roger Augusto <rogeraugusto206@gmail.com>
2026-09-09 20:28:57 -0300

    checkpoint antes de mudancas de JWT/observabilidade

--- OficinaMecanicaWagyu/obj/Debug/net10.0/OficinaMecanicaWagyu.AssemblyInfo.cs
+++ OficinaMecanicaWagyu/obj/Debug/net10.0/OficinaMecanicaWagyu.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OficinaMecanicaWagyu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ceada7d7cee78ec893a8241a50e35dea81501be1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4466595f385a75bc3bfe64103729a8f4c83c322")]
 [assembly: System.Reflection.AssemblyProductAttribute("OficinaMecanicaWagyu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OficinaMecanicaWagyu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
